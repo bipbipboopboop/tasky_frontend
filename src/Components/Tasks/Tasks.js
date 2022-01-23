671c09edfb95e0e9590d4821910a15f432a221bc
Your Name <ethanchen2009@hotmail.com>
2022-01-23 18:10:26 +0800

    Added tagging feature

--- src/Components/Tasks/Tasks.js
+++ src/Components/Tasks/Tasks.js
@@ -1,62 +1,115 @@
-import React, { useEffect, useState } from "react";
+import React, { Component } from "react";
 import Task from "./Task";
 
-const Tasks = () => {
-  const [items, setItems] = useState();
-  useEffect(() => {
-    fetch(`http://localhost:3001/api/v1/tasks`)
+const api_url = `http://localhost:3001/api/v1/tasks`;
+
+class Tasks extends Component {
+  //awake()
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     items: [],
+  //   };
+  //   this.deleteItem = this.deleteItem.bind(this);
+  // }
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      searchTerm: "",
+    };
+    this.deleteItem = this.deleteItem.bind(this);
+    this.setSearchTerm = this.setSearchTerm.bind(this);
+  }
+  // start()
+
+  componentDidMount() {
+    this.getTasks();
+  }
+
+  getTasks() {
+    fetch(api_url)
       .then((response) => response.json())
       .then((response_items) => {
-        console.log(response_items);
-        setItems(response_items);
+        this.setState({
+          // items: response_items.reverse(),
+          items: response_items,
+        });
       });
-  }, []);
+  }
 
-  //DELETE Request
-  const deleteItem = (item) => {
-    var deleteURL = `http://localhost:3001/api/v1/tasks` + `/${item.id}`;
+  deleteItem(item) {
+    var deleteURL = api_url + `/${item.id}`;
     fetch(deleteURL, {
       method: "DELETE",
     }).then(() => {
       // Client side delete
       //To be completed
-      var _items = items;
+      var _items = this.state.items;
       var index = _items.indexOf(item);
       _items.splice(index, 1);
-      setItems(_items);
+      this.setState({
+        items: _items,
+      });
     });
-  };
-  return (
-    <div
-      className="d-flex flex-column"
-      style={{
-        // position: "absolute",
-        // left: "40%",
-        justifyContent: "center",
-        width: "100%",
-        backgroundColor: "#f7e7ce",
-      }}
-    >
-      <h2>Tasks to be completed</h2>
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <form className="d-flex">
-          <input
-            className="form-control me-2"
-            type="search"
-            placeholder="Search for Task"
-            aria-label="Search for Task"
-          />
-        </form>
-      </ul>
-
-      {items &&
-        items.map((item) => (
+  }
+  setSearchTerm(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
+  render() {
+    return (
+      <div
+        className="d-flex flex-column"
+        style={{
+          // position: "absolute",
+          // left: "40%",
+          justifyContent: "center",
+          width: "100%",
+          backgroundColor: "#f7e7ce",
+        }}
+      >
+        <h2>Tasks to be completed</h2>
+        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          {/* <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  Calendar
+                </a>
+              </li> */}
+          <form className="d-flex">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search for Task"
+              aria-label="Search for Task"
+              onChange={this.setSearchTerm}
+            />
+          </form>
+        </ul>
+        {/* {this.state.items.map((item) => (
           <div className="p-2">
-            <Task key={item.id} item={item} deleteItem={deleteItem} />
+            <Task key={item.id} item={item} deleteItem={this.deleteItem} />
           </div>
-        ))}
-    </div>
-  );
-};
+        ))} */}
+        {this.state.items
+          .filter((val) => {
+            if (this.state.searchTerm == "") {
+              return val;
+            } else if (
+              val.title
+                .toLowerCase()
+                .includes(this.state.searchTerm.toLowerCase())
+            ) {
+              return val;
+            }
+          })
+          .map((item) => (
+            <div className="p-2">
+              <Task key={item.id} item={item} deleteItem={this.deleteItem} />
+            </div>
+          ))}
+      </div>
+    );
+  }
+}
 
 export default Tasks;
