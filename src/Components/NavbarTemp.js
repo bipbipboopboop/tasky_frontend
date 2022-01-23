d272d72b838da1c3ded14606e050ad0e1263b103
Your Name <ethanchen2009@hotmail.com>
2022-01-23 16:37:37 +0800

    Refactored Tasks.js

--- src/Components/NavbarTemp.js
+++ src/Components/NavbarTemp.js
@@ -21,8 +21,28 @@ const Navbar = () => {
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0"></ul>
-
+          {isLoggedIn ? (
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              {/* <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  Calendar
+                </a>
+              </li> */}
+              <form className="d-flex">
+                <input
+                  className="form-control me-2"
+                  type="search"
+                  placeholder="Search for Task"
+                  aria-label="Search for Task"
+                />
+                <button className="btn btn-outline-success" type="submit">
+                  Search
+                </button>
+              </form>
+            </ul>
+          ) : (
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0"></ul>
+          )}
           {isLoggedIn ? (
             <>
               <Link to="/newtask" className="btn btn-outline-success mx-2">
--- src/Components/Tasks/Tasks.js
+++ src/Components/Tasks/Tasks.js
@@ -1,82 +1,62 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import Task from "./Task";
 
-const api_url = `http://localhost:3001/api/v1/tasks`;
-
-class Tasks extends Component {
-  //awake()
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      items: [],
-    };
-    this.deleteItem = this.deleteItem.bind(this);
-    this.viewItem = this.viewItem.bind(this);
-  }
-  // start()
-
-  componentDidMount() {
-    this.getTasks();
-  }
-  viewItem(item) {
-    var viewURL = api_url + `/${item.id}`;
-    fetch(viewURL).then((response) => {
-      console.log(response.json());
-    });
-  }
-  getTasks() {
-    fetch(api_url)
+const Tasks = () => {
+  const [items, setItems] = useState();
+  useEffect(() => {
+    fetch(`http://localhost:3001/api/v1/tasks`)
       .then((response) => response.json())
       .then((response_items) => {
-        this.setState({
-          // items: response_items.reverse(),
-          items: response_items,
-        });
+        console.log(response_items);
+        setItems(response_items);
       });
-  }
+  }, []);
 
-  deleteItem(item) {
-    var deleteURL = api_url + `/${item.id}`;
+  //DELETE Request
+  const deleteItem = (item) => {
+    var deleteURL = `http://localhost:3001/api/v1/tasks` + `/${item.id}`;
     fetch(deleteURL, {
       method: "DELETE",
     }).then(() => {
       // Client side delete
       //To be completed
-      var _items = this.state.items;
+      var _items = items;
       var index = _items.indexOf(item);
       _items.splice(index, 1);
-      this.setState({
-        items: _items,
-      });
+      setItems(_items);
     });
-  }
+  };
+  return (
+    <div
+      className="d-flex flex-column"
+      style={{
+        // position: "absolute",
+        // left: "40%",
+        justifyContent: "center",
+        width: "100%",
+        backgroundColor: "#f7e7ce",
+      }}
+    >
+      <h2>Tasks to be completed</h2>
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <form className="d-flex">
+          <input
+            className="form-control me-2"
+            type="search"
+            placeholder="Search for Task"
+            aria-label="Search for Task"
+          />
+        </form>
+      </ul>
 
-  viewItem(item) {
-    var itemURL = api_url + `/${item.id}`;
-    return itemURL;
-  }
-  render() {
-    return (
-      <div
-        className="d-flex flex-column"
-        style={{
-          // position: "absolute",
-          // left: "40%",
-          justifyContent: "center",
-          width: "100%",
-          backgroundColor: "#f7e7ce",
-        }}
-      >
-        <h2>Tasks to be completed</h2>
-        {this.state.items.map((item) => (
+      {items &&
+        items.map((item) => (
           <div className="p-2">
-            <Task key={item.id} item={item} deleteItem={this.deleteItem} />
+            <Task key={item.id} item={item} deleteItem={deleteItem} />
           </div>
         ))}
-      </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Tasks;
